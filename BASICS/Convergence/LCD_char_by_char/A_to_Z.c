7680873e06c45a904bbf4977fafcb98aaaa6a3c7
Hemanth Kumar Giduturi <134470930+HeMaNtH624@users.noreply.github.com>
2025-11-17 08:43:46 +0530

    Add files via upload

---
+++ BASICS/Convergence/LCD_char_by_char/A_to_Z.c
@@ -0,0 +1,90 @@
+#include<LPC21XX.H>
+
+#include "delay.h"
+
+#define LCD_D 0xFF
+
+#define RS 1<<8
+
+#define E 1<<9
+
+
+void LCD_INIT(void);
+
+void LCD_CMD(unsigned char);
+
+void LCD_DATA(unsigned char);
+
+
+int main()
+
+{
+	int i;
+
+	LCD_INIT();
+	while(1)
+	{
+
+		LCD_CMD(0x80);
+		for(i='A'; i<='Z'; i++)
+		{
+			LCD_DATA('A');
+			delay(500);
+		}
+	}
+
+}
+
+
+void LCD_INIT(void)
+
+{
+
+	IODIR0 = (LCD_D|RS)|E;
+
+	LCD_CMD(0x01);
+
+	LCD_CMD(0x02) ;
+
+	LCD_CMD(0xc);
+
+	LCD_CMD(0x38);
+
+}
+
+
+void LCD_CMD(unsigned char cmd)
+
+{
+
+	IOPIN0 = cmd;
+
+	IOCLR0 = RS;
+
+	IOSET0 = E;
+
+	delay(500);
+
+	IOCLR0 = E;
+
+}
+
+
+void LCD_DATA(unsigned char d)
+
+{
+
+	IOPIN0 = d;
+
+	IOSET0 = RS;
+
+	IOSET0 = E;
+
+	delay(500);
+
+	IOCLR0 = E;
+
+}
+
+
+
---
+++ BASICS/Convergence/LCD_char_by_char/delay.h
@@ -0,0 +1,6 @@
+void delay(int n)
+{
+	int i;
+	for(; n>0; n--)
+	for(i=12000; i>=0; i--);
+}
